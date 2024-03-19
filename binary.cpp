d67604bda135eb0533d84ec115bd493ff307afbf
abhijitttt <112143922+abhijitttt@users.noreply.github.com>
2024-03-19 13:51:37 +0600

    Update binary.cpp

--- binary.cpp
+++ binary.cpp
@@ -1,15 +1,15 @@
  while (left <= right) {
         int mid = left + (right - left) / 2;
 
-        // If the element is present at the middle itself
+        
         if (arr[mid] == target)
             return mid;
 
-        // If the element is smaller than mid, then it can only be present in left subarray
+        
         if (arr[mid] < target)
             left = mid + 1;
 
-        // If the element is larger than mid, then it can only be present in right subarray
+       
         else
             right = mid - 1;
     }
