b43292b6f3a0faae39f316e66a1a7425a2f337ca
abhijitttt <112143922+abhijitttt@users.noreply.github.com>
2024-03-29 01:13:29 +0600

    max_sum_subarray.cpp

--- max_sum_subarray.cpp
+++ max_sum_subarray.cpp
@@ -1,13 +1,19 @@
-int max_sum(int a[], int size) {
-    int max_so_far = INT_MIN, max_ending_here = 0;
-    for (int i = 0; i < size; i++) {
-        max_ending_here += a[i];
-        if (max_so_far < max_ending_here) {
-            max_so_far = max_ending_here;
+int max_sum(int a[], int size) 
+{
+    int mx = INT_MIN;
+    int x = 0;
+    
+    for (int i = 0; i < size; i++) 
+    {
+        x += a[i];
+        if (mx < x) 
+        {
+            mx = x;
         }
-        if (max_ending_here < 0) {
-            max_ending_here = 0;
+        if (x < 0) 
+        {
+            x = 0;
         }
     }
-    return max_so_far;
+    return mx;
 }
