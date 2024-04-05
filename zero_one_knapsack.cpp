0fd05aa11a39e284b39ea3c2176062bf86eaf347
abhijitttt <112143922+abhijitttt@users.noreply.github.com>
2024-04-06 01:00:06 +0600

    Update zero_one_knapsack.cpp

--- zero_one_knapsack.cpp
+++ zero_one_knapsack.cpp
@@ -1,4 +1,4 @@
-int zero_one_knapsack(vector<int> wt,vector<int> val,int w,int n )
+int zero_one_knapsack(vector<int> wt,vector<int> val,int w,int n )//simple recursion
 {
     if(w == 0 || n == 0) return 0;
 
@@ -9,5 +9,61 @@ int zero_one_knapsack(vector<int> wt,vector<int> val,int w,int n )
     else
     {
         return zero_one_knapsack(wt,val,w,n-1);
+    }
+}
+
+
+
+//
+//MEMOIZATION
+#include <bits/stdc++.h>
+
+
+using namespace std;
+#define int long long
+#define endl "\n"
+int mod = 1e9+7;
+    
+int zero_one_knapsack(vector<int> wt,vector<int> val,vector<vector<int>> x,int w,int n )//using memoization(as fast as top down )                                
+{
+    
+    if(w == 0 || n == 0) return 0;
+    if(x[n-1][w-1] != -1)
+        return x[n-1][w-1];
+    if(wt[n-1] <= w)
+    {
+        return x[n-1][w-1] = max(val[n-1] + zero_one_knapsack(wt,val,x,w-wt[n-1],n-1),zero_one_knapsack(wt,val,x,w,n-1));
+    }
+    else
+    {
+        return x[n-1][w-1] = zero_one_knapsack(wt,val,x,w,n-1);
+    }
+}
+
+
+int32_t main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int tt;
+    cin >> tt;
+    while(tt--)
+    {
+        int n = 4;
+        int w = 7;
+        vector<int> wt{1,3,4,5};
+        vector<int> val{1,4,5,7};
+        vector<vector<int>> num(n+1,vector<int>(w+1));
+        for(int i = 0; i <= n; i++)
+        {
+            for(int j = 0; j <= w; j++)
+            {
+                num[i][j] = -1;
+            }
+        }
+
+        int ans = zero_one_knapsack(wt,val,num,w,n);
+        cout << ans << endl;
     }
 }
